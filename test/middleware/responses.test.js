1bbca63438ba922d26033a456948b3e5825c3cd5
Albert Martin <amartin@treehouse.co>
2016-07-08 16:51:28 -0500

    feat(responses): add response negotiation for all response types

--- test/middleware/responses.test.js
+++ test/middleware/responses.test.js
@@ -1,5 +1,6 @@
 process.env.NODE_ENV = 'test';
 
+const expect = require('expect');
 const request = require('supertest');
 const Nautilus = require('../../index');
 
@@ -14,7 +15,7 @@ describe('middleware:responses', function() {
   nautilus.app.get('/response/ok', (req, res) => res.ok('ok'));
   nautilus.app.get('/response/redirect', (req, res) => res.redirect('http://google.com', true));
   nautilus.app.get('/response/redirectPermanent', (req, res) => res.redirect('http://apple.com'));
-  nautilus.app.get('/response/error', (req, res) => res.serverError('error'));
+  nautilus.app.get('/response/error', (req, res) => res.serverError('server error'));
 
   before(done => nautilus.start(done));
 
@@ -26,6 +27,51 @@ describe('middleware:responses', function() {
   it('res.redirect', done => request(nautilus.app).get('/response/redirect/').expect(302, done));
   it('res.redirect (permanent)', done => request(nautilus.app).get('/response/redirectPermanent/').expect(301, done));
   it('res.serverError', done => request(nautilus.app).get('/response/error/').expect(500, done));
+
+  describe('content-type negotiation', function() {
+    it('uses HTML when appropriate', done => {
+      request(nautilus.app)
+        .get('/response/error/')
+        .set('Accept', 'text/html')
+        .expect(500, (err, response) => {
+          expect(response.type).toEqual('text/html');
+          expect(response.text).toEqual('server error');
+          done(err);
+        });
+    });
+
+    it('uses JSON when appropriate', done => {
+      request(nautilus.app)
+        .get('/response/error/')
+        .set('Accept', 'application/json')
+        .expect(500, (err, response) => {
+          expect(response.type).toEqual('application/json');
+          expect(response.body).toEqual({ error: 'server error' });
+          done(err);
+        });
+    });
+
+    it('interchanges `error` for `body` based on response code', done => {
+      request(nautilus.app)
+        .get('/response/ok/')
+        .set('Accept', 'application/json')
+        .expect(200, (err, response) => {
+          expect(response.body).toEqual({ body: 'ok' });
+          done(err);
+        });
+    });
+
+    it('uses TXT when appropriate', done => {
+      request(nautilus.app)
+        .get('/response/error/')
+        .set('Accept', 'none')
+        .expect(500, (err, response) => {
+          expect(response.type).toEqual('text/plain');
+          expect(response.text).toEqual('server error');
+          done(err);
+        });
+    });
+  });
 
   after(() => nautilus.server.close());
 
