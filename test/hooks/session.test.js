0ea802e05fd80b25315f293b0f970abbe00f2536
Albert Martin <albert@imor.tl>
2016-08-18 15:31:01 -0500

    fix(session): correct typed strict equals to disable

--- test/hooks/session.test.js
+++ test/hooks/session.test.js
@@ -5,37 +5,58 @@ const Nautilus = require('../../index');
 
 describe('hooks:session', function() {
 
-  var nautilus = new Nautilus({
-    routes: {
-      '/session': function(req, res) {
-        if (!req.session) return res.serverError();
-        var count = req.session.count || 1;
-        req.session.count = count + 1;
-        res.ok(`Session visits: ${count}`);
-      }
+  var routes = {
+    '/session': function(req, res) {
+      if (!req.session) return res.serverError();
+      var count = req.session.count || 1;
+      req.session.count = count + 1;
+      res.ok(`Session visits: ${count}`);
     }
-  });
-  before(done => nautilus.start(done));
+  };
 
-  var session;
+  describe('enabled', function() {
 
-  it('assigns a cookie with session ID', function(done) {
-    request(nautilus.app).get('/session/').expect(200).end((err, response) => {
-      expect(response.text).toEqual('Session visits: 1');
-      var sessionCookie = cookie.parse(response.headers['set-cookie'][0]);
-      expect(sessionCookie['nautilus.sid']).toExist();
-      session = sessionCookie['nautilus.sid'];
-      done(err);
+    var nautilus = new Nautilus({
+      routes
     });
-  });
+    before(done => nautilus.start(done));
 
-  it('respects the "Cookie" header', function(done) {
-    request(nautilus.app).get('/session/').set('Cookie', `nautilus.sid=${session}`).end((err, response) => {
-      expect(response.text).toEqual('Session visits: 2');
-      done(err);
+    var session;
+
+    it('assigns a cookie with session ID', function(done) {
+      request(nautilus.app).get('/session/').expect(200).end((err, response) => {
+        expect(response.text).toEqual('Session visits: 1');
+        var sessionCookie = cookie.parse(response.headers['set-cookie'][0]);
+        expect(sessionCookie['nautilus.sid']).toExist();
+        session = sessionCookie['nautilus.sid'];
+        done(err);
+      });
     });
+
+    it('respects the "Cookie" header', function(done) {
+      request(nautilus.app).get('/session/').set('Cookie', `nautilus.sid=${session}`).end((err, response) => {
+        expect(response.text).toEqual('Session visits: 2');
+        done(err);
+      });
+    });
+
+    after(() => nautilus.server.close());
+
   });
 
-  after(() => nautilus.server.close());
+  describe('disabled', function() {
+
+    var nautilus = new Nautilus({
+      session: false,
+      routes
+    });
+    before(done => nautilus.start(done));
+
+    it('respects the settings in configuration', () =>
+      request(nautilus.app).get('/session/').expect(500));
+
+    after(() => nautilus.server.close());
+
+  });
 
 });
