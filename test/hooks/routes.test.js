c38d387d92dce45ca68acd45c4fd7f4234a7ee9d
Albert Martin <albert@imor.tl>
2016-10-19 11:13:37 -0500

    test(routes): add additional tests for index and regex routes

--- test/hooks/routes.test.js
+++ test/hooks/routes.test.js
@@ -1,3 +1,4 @@
+const expect = require('expect');
 const request = require('supertest');
 const Nautilus = require('../../index');
 
@@ -6,14 +7,34 @@ describe('hooks:routes', function() {
   var nautilus = new Nautilus({
     slash: false,
     routes: {
+      'index': (req, res) => res.ok('home'),
       '/hello': function(req, res) {
-        res.ok('world');
+        res.ok(req.params.id);
+      },
+      'r|(foo|bar)': function(req, res) {
+        res.ok(req.params[0]);
       }
     }
   });
   before(done => nautilus.start(done));
 
   it('allows routes to be declared in configuration', () => request(nautilus.app).get('/hello').expect(200));
+
+  it('includes an optional ID parameter in route', done => {
+    request(nautilus.app).get('/hello/world').end(function(err, res) {
+      expect(res.text).toEqual('world');
+      done(err);
+    });
+  });
+
+  it('uses the `index` route for root requests', () => request(nautilus.app).get('/').expect(200));
+
+  it('allows routes to be declared as regex', done => {
+    request(nautilus.app).get('/foo').end(function(err, res) {
+      expect(res.text).toEqual('foo');
+      done(err);
+    });
+  });
 
   after(done => nautilus.stop(done));
 
