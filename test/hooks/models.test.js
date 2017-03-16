e703bb7b9a34aebb5305fb66fc17cb9da1a49d66
Albert Martin <albert@imor.tl>
2017-03-16 17:41:48 -0500

    feat(models): add shortcut to mongoose model

--- test/hooks/models.test.js
+++ test/hooks/models.test.js
@@ -34,6 +34,29 @@ describe('hooks:models', function() {
     expect(nautilus.app.config.foo).toEqual('bar');
   });
 
+  describe('shortcut actions', () => {
+    let record;
+    let fixture = {
+      email: 'test@test.com',
+      name: 'Test User'
+    };
+
+    it('exposes find shortcuts for all models', () => {
+      return nautilus.app.api.model.user.create(fixture, () => {
+        return nautilus.app.model('user').find({ email: fixture.email });
+      }).then(result => {
+        record = result;
+        expect(result.email).toEqual(fixture.email);
+      });
+    });
+
+    it('allows findOne criteria to be an ObjectID', () => {
+      return nautilus.app.model('user').findOne(record._id).then(result => {
+        expect(result.email).toEqual(fixture.email);
+      });
+    });
+  });
+
   after(done => nautilus.stop(done));
 
 });
