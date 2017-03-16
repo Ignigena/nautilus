f6b861884f934bd93a559b7b12e001e152456b09
Albert Martin <albert@imor.tl>
2017-03-16 15:13:15 -0500

    test(models): add basic tests for schema setup

---
+++ test/hooks/models.test.js
@@ -0,0 +1,36 @@
+const expect = require('expect');
+const request = require('supertest');
+const Nautilus = require('../../index');
+
+describe('hooks:models', function() {
+
+  var nautilus = new Nautilus({
+    connections: {
+      mongo: { url: process.env.DB_MONGO || 'mongodb://127.0.0.1:27017' }
+    },
+    models: {
+      user: {
+        schema: {
+          email: String,
+          name: String,
+        },
+        setup(schema, app) {
+          app.config.foo = 'bar';
+        }
+      }
+    }
+  });
+  before(done => nautilus.start(done));
+
+  it('sets up model definitions', () => {
+    expect(nautilus.app.api.model).toExist();
+    expect(nautilus.app.api.model.user).toExist();
+  });
+
+  it('executes the setup function after initialization', () => {
+    expect(nautilus.app.config.foo).toEqual('bar');
+  });
+
+  after(done => nautilus.stop(done));
+
+});
