3995c73e11a49b80133fcb4ce73ded5555fa00a8
Albert Martin <albert@imor.tl>
2019-10-12 20:49:56 -0500

    feat(micro): add response helpers

---
+++ packages/micro/lib/response.js
@@ -0,0 +1,27 @@
+const { STATUS_CODES } = require('http')
+
+const camelcase = require('camelcase')
+const { send } = require('micro')
+
+const codes = [
+  ...Object.entries(STATUS_CODES),
+  [301, 'Redirect'],
+  [420, 'Slow Your Roll'],
+  [500, 'Error']
+]
+
+function sendStatus (res, statusCode, message) {
+  if (typeof message !== 'object') {
+    message = { [statusCode >= 400 ? 'error' : 'data']: message }
+  }
+  send(res, statusCode, message)
+}
+
+module.exports = next => (req, res) => {
+  codes.reduce((response, [statusCode, verb]) => {
+    response[camelcase(verb)] = data => sendStatus(res, statusCode, data || verb)
+    return response
+  }, res)
+
+  next(req, res)
+}
---
+++ packages/micro/test/response.test.js
@@ -0,0 +1,31 @@
+const expect = require('expect')
+const { describe, it } = require('mocha')
+const request = require('supertest')
+
+const micro = require('micro')
+
+const response = require('../lib/response')
+
+describe('response', () => {
+  it('adds response helpers for common status codes', async () => {
+    await Promise.all([
+      request(micro(response((req, res) => res.ok()))).get('/').expect(200),
+      request(micro(response((req, res) => res.created()))).get('/').expect(201),
+      request(micro(response((req, res) => res.redirect()))).get('/').expect(301),
+      request(micro(response((req, res) => res.badRequest()))).get('/').expect(400),
+      request(micro(response((req, res) => res.forbidden()))).get('/').expect(403),
+      request(micro(response((req, res) => res.notFound()))).get('/').expect(404),
+      request(micro(response((req, res) => res.error()))).get('/').expect(500)
+    ])
+  })
+
+  it('uses the correct `data` or `error` key in the top level', async () => {
+    const good = await request(micro(response((req, res) => res.ok()))).get('/')
+    expect(good.body.data).toBeDefined()
+    expect(good.body.error).not.toBeDefined()
+
+    const bad = await request(micro(response((req, res) => res.badRequest()))).get('/')
+    expect(bad.body.data).not.toBeDefined()
+    expect(bad.body.error).toBeDefined()
+  })
+})
