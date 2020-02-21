e79badbb1abadc8baac5e71de3032f9b85fc70e5
Albert Martin <albert@imor.tl>
2020-02-20 19:58:48 -0600

    feat(micro): add generic middleware stack helper

--- packages/micro/index.js
+++ packages/micro/index.js
@@ -9,6 +9,14 @@ const hooks = [
   'models'
 ]
 
+/**
+ * Zero-config handler for most out-of-the-box setups. You should use this if
+ * you'll use most of the functionality that comes built in to the framework.
+ * @param {Function} next - The last middleware handler that will be called.
+ * @param {Object} config - Optional configuration, otherwise configuration
+ * will be loaded from the closest `config` directory in the app.
+ * @return {Function} A request/response handler.
+ */
 exports.nautilus = (next, config) => {
   if (!config) {
     const foundConfig = findUp.sync('config', {
@@ -18,7 +26,19 @@ exports.nautilus = (next, config) => {
     config = foundConfig ? this.utils.config(foundConfig) : {}
   }
 
-  return hooks.filter(hook => config[hook] !== false).reverse()
-    .map(m => require('./lib/' + m))
-    .reduce((stack, hook) => hook(stack, config), next)
+  return exports.withMiddleware(hooks.filter(hook => config[hook] !== false))(next, config)
+}
+
+/**
+ * For more advanced setups, the `withMiddleware` option comes with no assumptions
+ * about your approach. You can intermix your own hooks with the core ones based
+ * on the needs of your application.
+ * @param {Array<String|Function>} hooks - If a string is provided, it is assumed
+ * to be a core Nautilus hook. Otherwise, a function or `require` should be used
+ * to define custom hooks.
+ * @return {Function} The middleware stack, ready to wrap your handler.
+ */
+exports.withMiddleware = hooks => {
+  const stack = hooks.reverse().map(m => (typeof m === 'string') ? require('./lib/' + m) : m)
+  return (next, ...args) => stack.reduce((stack, hook) => hook(stack, ...args), next)
 }
